5e1fababdd8c204eb3835e510fca159fef61c262
JosephWYZ <josephwu.wyz@hotmail.com>
2016-05-26 10:22:17 -0700

    Create a cluster with 1 master and 4 slaves; WordCount example works

--- start-container.sh
+++ start-container.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
+eval "$(docker-machine env default)"
 # run N slave containers
 N=$1
 
 # the defaut node number is 3
 if [ $# = 0 ]
 then
-	N=3
+	N=5
 fi
-	
+
+MASTER_IMAGE=yiw376/hadoop-master
+SLAVE_IMAGE=yiw376/hadoop-slave
+HOST_POSTFIX=yiw376.eng.ucsd.edu
+MASTER_HOST=master.yiw376.eng.ucsd.edu
+MASTER_CONTAINER=master
+SLAVE_CONTAINER_PREFIX=slave
 
 # delete old master container and start new master container
-sudo docker rm -f master &> /dev/null
+docker rm -f master &> /dev/null
 echo "start master container..."
-sudo docker run -d -t --dns 127.0.0.1 -P --name master -h master.kiwenlau.com -w /root kiwenlau/hadoop-master:0.1.0 &> /dev/null
+docker run -d -t --dns 127.0.0.1 -P --name $MASTER_CONTAINER -h $MASTER_HOST -w /root $MASTER_IMAGE &> /dev/null
 
 # get the IP address of master container
-FIRST_IP=$(sudo docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
+FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
 
 # delete old slave containers and start new slave containers
 i=1
 while [ $i -lt $N ]
 do
-	sudo docker rm -f slave$i &> /dev/null
+	docker rm -f $SLAVE_CONTAINER_PREFIX$i &> /dev/null
 	echo "start slave$i container..."
-	sudo docker run -d -t --dns 127.0.0.1 -P --name slave$i -h slave$i.kiwenlau.com -e JOIN_IP=$FIRST_IP kiwenlau/hadoop-slave:0.1.0 &> /dev/null
+	docker run -d -t --dns 127.0.0.1 -P --name $SLAVE_CONTAINER_PREFIX$i -h $SLAVE_CONTAINER_PREFIX$i.$HOST_POSTFIX -e JOIN_IP=$FIRST_IP $SLAVE_IMAGE &> /dev/null
 	i=$(( $i + 1 ))
-done 
+done
 
 
 # create a new Bash session in the master container
-sudo docker exec -it master bash
+docker exec -it $MASTER_CONTAINER bash
